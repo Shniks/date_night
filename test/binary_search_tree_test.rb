1a474f73ec42dbc9aad1730b151e6de4b3082b49
Lihkin <shniks@gmail.com>
2018-01-27 05:57:08 -0700

    Refactoring binary search tree test to include a setup method.

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -4,16 +4,16 @@ require './lib/binary_search_tree'
 
 class BinarySearchTreeTest < Minitest::Test
 
-  def test_if_it_exists
-    tree = BinarySearchTree.new
+  def setup
+    @tree = BinarySearchTree.new
+  end
 
-    assert_instance_of BinarySearchTree, tree
+  def test_if_it_exists
+    assert_instance_of BinarySearchTree, @tree
   end
 
   def test_if_head_node_is_initially_nil
-    tree = BinarySearchTree.new
-
-    assert_nil nil, tree.head
+    assert_nil nil, @tree.head
   end
 
 end
