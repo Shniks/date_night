5e3d760842694993390134449ae7a1557746a7eb
Lihkin <shniks@gmail.com>
2018-01-27 08:36:06 -0700

    Changing order of methods.

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -11,6 +11,24 @@ class BinarySearchTree
     @head = Node.new(score, title)
   end
 
+  def insert(score, title)
+    if @head.nil?
+      insert_first_node(score, title)
+      @depth = 0
+    else
+      score_direction(score, title)
+    end
+  end
+
+  def score_direction(score, title)
+    @current_node = @head
+    if score < @current_node.score
+      score_left(score, title)
+    else
+      score_right(score, title)
+    end
+  end
+
   def score_left(score, title)
     while @current_node.node_left != nil
       @current_node = @current_node.node_left
@@ -27,24 +45,7 @@ class BinarySearchTree
     @depth += 1
   end
 
-  def score_direction(score, title)
-    @current_node = @head
-    if score < @current_node.score
-      score_left(score, title)
-    else
-      score_right(score, title)
-    end
-    @depth
-  end
 
-  def insert(score, title)
-    if @head.nil?
-      insert_first_node(score, title)
-      @depth = 0
-    else
-      score_direction(score, title)
-    end
-  end
 
 
 
