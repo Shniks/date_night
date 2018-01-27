ebca4387e53437e840da48c5a788df96a27ed9c8
Lihkin <shniks@gmail.com>
2018-01-27 05:31:40 -0700

    Refactoring NodeTest class to create a setup method.

--- test/node_test.rb
+++ test/node_test.rb
@@ -4,34 +4,28 @@ require './lib/node'
 
 class NodeTest < Minitest::Test
 
-  def test_if_it_exists
-    node = Node.new(92, "Sharknado 3")
+  def setup
+    @node = Node.new(92, "Sharknado 3")
+  end
 
-    assert_instance_of Node, node
+  def test_if_it_exists
+    assert_instance_of Node, @node
   end
 
   def test_node_can_return_score
-    node = Node.new(92, "Sharknado 3")
-
-    assert_equal 92, node.score
+    assert_equal 92, @node.score
   end
 
   def test_node_can_return_title
-    node = Node.new(92, "Sharknado 3")
-
-    assert_equal "Sharknado 3", node.title
+    assert_equal "Sharknado 3", @node.title
   end
 
   def test_node_initialized_with_nil_left_next_node
-    node = Node.new(92, "Sharknado 3")
-
-    assert_nil nil, node.node_left
+    assert_nil nil, @node.node_left
   end
 
   def test_node_initialized_with_nil_right_next_node
-    node = Node.new(92, "Sharknado 3")
-
-    assert_nil nil, node.node_right
+    assert_nil nil, @node.node_right
   end
 
 end
