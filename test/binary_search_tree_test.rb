9c6baa61202507f30d2a7b1e81eea0e61803aec3
Lihkin <shniks@gmail.com>
2018-01-27 22:40:57 -0700

    Adding test for depth_of method.

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -33,14 +33,14 @@ class BinarySearchTreeTest < Minitest::Test
     assert_nil nil, result.node_right
   end
 
-  def test_it_can_insert_and_find_depth_for_a_node
+  def test_it_can_insert_and_find_depth_of_a_node
     result = @tree.insert(61, "Bill & Ted's Excellent Adventure")
 
     assert_equal 0, result
     refute_equal 1, result
   end
 
-  def test_it_can_insert_and_find_depth_for_a_second_node_to_the_left
+  def test_it_can_insert_and_find_depth_of_a_second_node_to_the_left
     @tree.insert(61, "Bill & Ted's Excellent Adventure")
     result = @tree.insert(16, "Johnny English")
 
@@ -48,7 +48,7 @@ class BinarySearchTreeTest < Minitest::Test
     refute_equal 0, result
   end
 
-  def test_it_can_insert_and_find_depth_for_a_third_node_to_the_right
+  def test_it_can_insert_and_find_depth_of_a_third_node_to_the_right
     @tree.insert(61, "Bill & Ted's Excellent Adventure")
     @tree.insert(16, "Johnny English")
     result = @tree.insert(92, "Sharknado 3")
@@ -57,7 +57,7 @@ class BinarySearchTreeTest < Minitest::Test
     refute_equal 2, result
   end
 
-  def test_it_can_insert_and_find_depth_for_a_fourth_node
+  def test_it_can_insert_and_find_depth_of_a_fourth_node
     @tree.insert(61, "Bill & Ted's Excellent Adventure")
     @tree.insert(16, "Johnny English")
     @tree.insert(92, "Sharknado 3")
@@ -67,11 +67,23 @@ class BinarySearchTreeTest < Minitest::Test
     refute_equal 3, result
   end
 
-  def test_it_can_find_depth_for_each_movie_when_inserting_twelve_movies
+  def test_it_can_find_depth_of_each_movie_when_inserting_twelve_movies
     @movies_list.each do |key, value|
       result = @tree.insert(key, value[0])
       assert_equal value[1], result
     end
+  end
+
+  def test_it_can_find_depth_of_any_movie
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(16, "Johnny English")
+    @tree.insert(92, "Sharknado 3")
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal 1, @tree.depth_of(92)
+    refute_equal 2, @tree.depth_of(92)
+    assert_equal 2, @tree.depth_of(50)
+    refute_equal 3, @tree.depth_of(50)
   end
 
   def test_it_can_find_movie_with_max_score
@@ -89,7 +101,6 @@ class BinarySearchTreeTest < Minitest::Test
       result = @tree.insert(key, value[0])
     end
     expected = {"Transformers7"=>98}
-
     assert_equal expected, @tree.max
   end
 
