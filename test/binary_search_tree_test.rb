5500264336585cabefd5e09a629213a667fa7d92
Lihkin <shniks@gmail.com>
2018-01-27 22:19:26 -0700

    Refactoring test file to include a hash in the setup method that contains the movie, score and depth values instead of having them in each test method.

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -6,6 +6,13 @@ class BinarySearchTreeTest < Minitest::Test
 
   def setup
     @tree = BinarySearchTree.new
+    @movies_list = { 61 => ["Bill & Ted's Excellent Adventure", 0],
+    16 => ["Johnny English", 1], 92 => ["Sharknado 3", 1],
+    50 => ["Hannibal Buress: Animal Furnace", 2], 40 => ["Transformers1", 3],
+    45 => ["Transformers2", 4], 55 => ["Transformers3", 3],
+    94 => ["Transformers4", 2], 90 => ["Transformers5", 2],
+    35 => ["Transformers6", 4], 98 => ["Transformers7", 3],
+    70 => ["Transformers8", 3] }
   end
 
   def test_if_it_exists
@@ -30,6 +37,7 @@ class BinarySearchTreeTest < Minitest::Test
     result = @tree.insert(61, "Bill & Ted's Excellent Adventure")
 
     assert_equal 0, result
+    refute_equal 1, result
   end
 
   def test_it_can_insert_a_second_node_to_the_left
@@ -37,6 +45,7 @@ class BinarySearchTreeTest < Minitest::Test
     result = @tree.insert(16, "Johnny English")
 
     assert_equal 1, result
+    refute_equal 0, result
   end
 
   def test_it_can_insert_a_third_node_to_the_right
@@ -45,6 +54,7 @@ class BinarySearchTreeTest < Minitest::Test
     result = @tree.insert(92, "Sharknado 3")
 
     assert_equal 1, result
+    refute_equal 2, result
   end
 
   def test_it_can_insert_a_fourth_node
@@ -54,38 +64,14 @@ class BinarySearchTreeTest < Minitest::Test
     result = @tree.insert(50, "Hannibal Buress: Animal Furnace")
 
     assert_equal 2, result
+    refute_equal 3, result
   end
 
-  def test_it_can_insert_a_tenth_node
-    @tree.insert(61, "Bill & Ted's Excellent Adventure")
-    @tree.insert(16, "Johnny English")
-    @tree.insert(92, "Sharknado 3")
-    @tree.insert(50, "Hannibal Buress: Animal Furnace")
-    @tree.insert(40, "Transformers1")
-    @tree.insert(45, "Transformers2")
-    @tree.insert(55, "Transformers3")
-    @tree.insert(94, "Transformers4")
-    @tree.insert(90, "Transformers5")
-    result = @tree.insert(35, "Transformers6")
-
-    assert_equal 4, result
-  end
-
-  def test_it_can_insert_a_twelfth_node
-    @tree.insert(61, "Bill & Ted's Excellent Adventure")
-    @tree.insert(16, "Johnny English")
-    @tree.insert(92, "Sharknado 3")
-    @tree.insert(50, "Hannibal Buress: Animal Furnace")
-    @tree.insert(40, "Transformers1")
-    @tree.insert(45, "Transformers2")
-    @tree.insert(55, "Transformers3")
-    @tree.insert(94, "Transformers4")
-    @tree.insert(90, "Transformers5")
-    @tree.insert(35, "Transformers6")
-    @tree.insert(98, "Transformers7")
-    result = @tree.insert(70, "Transformers8")
-
-    assert_equal 3, result
+  def test_it_can_insert_twelve_nodes
+    @movies_list.each do |key, value|
+      result = @tree.insert(key, value[0])
+      assert_equal value[1], result
+    end
   end
 
   def test_it_can_find_movie_with_max_score
@@ -98,19 +84,10 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal expected, @tree.max
   end
 
-  def test_it_finds_another_movie_with_max_score
-    @tree.insert(61, "Bill & Ted's Excellent Adventure")
-    @tree.insert(16, "Johnny English")
-    @tree.insert(92, "Sharknado 3")
-    @tree.insert(50, "Hannibal Buress: Animal Furnace")
-    @tree.insert(40, "Transformers1")
-    @tree.insert(45, "Transformers2")
-    @tree.insert(55, "Transformers3")
-    @tree.insert(94, "Transformers4")
-    @tree.insert(90, "Transformers5")
-    @tree.insert(35, "Transformers6")
-    @tree.insert(98, "Transformers7")
-    @tree.insert(70, "Transformers8")
+  def test_it_finds_movie_with_max_score_among_twelve_movies
+    @movies_list.each do |key, value|
+      result = @tree.insert(key, value[0])
+    end
     expected = {"Transformers7"=>98}
 
     assert_equal expected, @tree.max
@@ -126,20 +103,11 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal expected, @tree.min
   end
 
-  def test_it_finds_another_movie_with_min_score
-    @tree.insert(61, "Bill & Ted's Excellent Adventure")
-    @tree.insert(92, "Sharknado 3")
-    @tree.insert(50, "Hannibal Buress: Animal Furnace")
-    @tree.insert(40, "Transformers1")
-    @tree.insert(45, "Transformers2")
-    @tree.insert(16, "Johnny English")
-    @tree.insert(55, "Transformers3")
-    @tree.insert(8, "Transformers7")
-    @tree.insert(94, "Transformers4")
-    @tree.insert(90, "Transformers5")
-    @tree.insert(35, "Transformers6")
-    @tree.insert(70, "Transformers8")
-    expected = {"Transformers7"=>8}
+  def test_it_finds_another_movie_with_min_score_among_twelve_movies
+    @movies_list.each do |key, value|
+      result = @tree.insert(key, value[0])
+    end
+    expected = {"Johnny English"=>16}
 
     assert_equal expected, @tree.min
   end
